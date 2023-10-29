07d4d3fd618133436010eacb40ba090b66b1fa44
KillerDogs <kenybustamante2@hotmail.com>
2023-10-29 02:28:35 -0300

    CORS update

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-const express = require('express')
-const categoryRoutes = require('./routes/categoryRoutes')
-const productRoutes = require('./routes/productRoutes')
-const serviceRoutes = require('./routes/serviceRoutes')
-const cors = require('cors')
-const redirectMiddleware = require('./middlewares/redirectMiddleware')
-const logMiddleware = require('./middlewares/logMiddleware')
-const rateLimit = require('./middlewares/rateLimit')
-
+const express = require('express');
+const categoryRoutes = require('./routes/categoryRoutes');
+const productRoutes = require('./routes/productRoutes');
+const serviceRoutes = require('./routes/serviceRoutes');
+const cors = require('cors');
+const redirectMiddleware = require('./middlewares/redirectMiddleware');
+const logMiddleware = require('./middlewares/logMiddleware');
+const rateLimit = require('./middlewares/rateLimit');
 
 const app = express();
 const port = 4000;
 
+const corsOptions = {
+  origin: 'https://tomepromo.com.br',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 app.use(express.json());
-app.use(cors())
-app.use(redirectMiddleware)
-app.use(logMiddleware)
-app.use(rateLimit)
+app.use(cors(corsOptions));
+app.use(redirectMiddleware);
+app.use(logMiddleware);
+app.use(rateLimit);
 
 app.get('/', (request, response) => {
-    response.send("Hello World")
-})
+  response.send('Hello World');
+});
 
-app.use('/categories', categoryRoutes )
-app.use('/products', productRoutes)
-app.use('/services', serviceRoutes)
+app.use('/categories', categoryRoutes);
+app.use('/products', productRoutes);
+app.use('/services', serviceRoutes);
 
-app.listen(port, () =>{
-    console.log('Servidor rodando em http://localhost:'+ port)
-})
+app.listen(port, () => {
+  console.log('Servidor rodando em http://localhost:' + port);
+});
