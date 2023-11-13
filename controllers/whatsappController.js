57d9aaf0ff2ecbad8fccd07fc88a0c33d922492a
KillerDogs <kenybustamante2@hotmail.com>
2023-11-13 16:35:44 -0300

    update library

--- controllers/whatsappController.js
+++ controllers/whatsappController.js
@@ -83,9 +83,7 @@ const sendMessageToWhatsApp = async (req, res) => {
     const chatId = phoneNumber;
     const chat = await client.getChatById(chatId);
     if (chat) {
-      const delay = (ms) => new Promise((res) => setTimeout(res, ms));
       await chat.sendStateTyping();
-      await delay(3000); // Espera 5 segundos
       await chat.sendMessage(message, {
         linkPreview: { includePreview: true },
       });
